d1d4a5413aa0e5950832ea2a5d3ac5390c559fdd
roknyazev <romaknyazev55@yandex.ru>
2021-11-09 23:40:21 +0300

    123

--- hubs/start_hubs.sh
+++
@@ -1,9 +0,0 @@
-apt-get update
-apt-get install net-tools
-apt-get install python3 -y
-apt install python3-pip -y
-
-pip3 install requests
-
-python3 main.py >> /hubs_log &
-cd ../
--- orderManager/schema/000001_init.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE order;
-
-DROP TABLE hub;
